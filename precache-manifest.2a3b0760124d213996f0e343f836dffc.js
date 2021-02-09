3a1c849e7da095401dd94e151b0e7699d6adcaaa
Vishvamitra <vishvamitrakumarsingh@gmail.com>
2021-02-09 19:54:03 +0530

    Updates

---
+++ precache-manifest.2a3b0760124d213996f0e343f836dffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bdbc0d6477190f01abf7663489289b",
+    "url": "/ga/index.html"
+  },
+  {
+    "revision": "6e6ff6beadf010fc6a3f",
+    "url": "/ga/static/css/main.dbebb9a4.chunk.css"
+  },
+  {
+    "revision": "abfd98f79862be45ef5c",
+    "url": "/ga/static/js/2.08c8c678.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/ga/static/js/2.08c8c678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6ff6beadf010fc6a3f",
+    "url": "/ga/static/js/main.b163f053.chunk.js"
+  },
+  {
+    "revision": "56db82aad232f8935079",
+    "url": "/ga/static/js/runtime-main.944d604b.js"
+  }
+]);
